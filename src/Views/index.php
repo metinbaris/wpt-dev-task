fcc9b6e6f7a29b443d8fc1c9ff6580f5009235c0
metinbaris <hizmetparki@gmail.com>
2024-05-28 18:05:52 +0300

    Create base view

---
+++ src/Views/index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dataset Information</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/picocss/2.0.0/pico.min.css">
+</head>
+
+<body>
+    <main class="container">
+        <h1>Dataset Information</h1>
+        <table>
+            <thead>
+                <tr>
+                    <th>Task</th>
+                    <th>Title</th>
+                    <th>Description</th>
+                    <th>ColorCode</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                foreach ($dataset as $data) {
+                    echo
+                        '<tr>' .
+                        '<td>' . $data->task . '</td>' .
+                        '<td>' . $data->title . '</td>' .
+                        '<td>' . $data->description . '</td>' .
+                        '<td style="color: ' . $data->colorCode . '">' . $data->colorCode . '</td>' .
+                        '</tr>';
+                }
+                ?>
+            </tbody>
+        </table>
+    </main>
+</body>
+
+</html>
