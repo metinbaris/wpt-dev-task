16a362ca69b9b44a040343076c3565e71302b9b6
metinbaris <hizmetparki@gmail.com>
2024-05-28 18:04:29 +0300

    Update routes.php
    - Add dataset service to dataset controller, called redis

--- src/routes.php
+++ src/routes.php
@@ -4,6 +4,7 @@ use Metinbaris\Vero\Controllers\DatasetController;
 use Metinbaris\Vero\Services\DatasetService;
 use Metinbaris\Vero\Cache\RedisCache;
 use Predis\Client;
+use GuzzleHttp\Client as HttpClient;
 
 $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 
@@ -11,13 +12,14 @@ $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 if ($uri === "/") {
     $redisClient = new Client([
         'scheme' => 'tcp',
-        'host'   => 'redis',
-        'port'   => 6379,
+        'host' => 'redis',
+        'port' => 6379,
     ]);
     $cache = new RedisCache($redisClient);
-    $datasetService = new DatasetService($cache);    
+    $guzzle = new HttpClient(['base_uri' => DatasetService::CONSTANT_BASE_URI]);
+    $datasetService = new DatasetService($cache, $guzzle);
     $datasetController = new DatasetController($datasetService);
-    
+
     $datasetController->index();
 } else {
     // Route not found
